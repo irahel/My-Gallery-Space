cda3d9ae2d9d3ba3707637515c858922480d09b5
Irahel <isaac_rahel@live.com>
2023-02-27 13:37:35 -0300

    feat: add new font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './assets/fonts/GandhiSans-Regular.otf'
+import './assets/fonts/GandhiSans-Bold.otf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
