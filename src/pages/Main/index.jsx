f4655203ce7e71b60cc17694479fd6a6f5085ae6
Irahel <isaac_rahel@live.com>
2023-02-27 10:18:34 -0300

    feat: add banner

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,22 @@
-import Header from "../../components/Header"
-import Menu from "../../components/Menu"
+import Header from "../../components/Header";
+import Menu from "../../components/Menu";
+import Banner from './banner.png';
+import styles from './Main.module.scss';
 
 function Main() {
   return (
     <>
     <Header/>
-    <Menu />
-    <div>Main</div>
+    <main>
+      <section className={styles.main}>
+        <Menu />
+        <div className={styles.main__img}>
+          <h1>A galeria mais completa do espaço</h1>
+          <img src={Banner} alt="A terra vista do espaço" />
+        </div>
+      </section>
+    </main>
+
     </>
   )
 }
